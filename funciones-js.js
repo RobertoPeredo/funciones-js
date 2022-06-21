6547ac789b755889c51ac467549cc6e41753d080
RobertoPeredo <99369122+RobertoPeredo@users.noreply.github.com>
2022-06-21 08:21:21 -0500

    Add files via upload

---
+++ funciones-js.js
@@ -0,0 +1,17 @@
+// Funcion declarada
+function sumar( n1, n2){
+    return n1+n2;
+}
+
+// Función expresada
+const sumar = function(n1, n2){
+    return n1+n2;
+}
+
+// Arrow function- Expresió
+const sumar= (n1,n2)=>{
+    return n1+n2;
+}
+
+// Arrow function- Retorno implícito
+const sumar= (n1,n2) => n1+n2;
